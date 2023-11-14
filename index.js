4e4f9156bb7c946a33010c9dbc7f082c2051bb28
gowthamkish <gowthamkish@outlook.com>
2023-11-14 10:03:14 +0530

    Fixing prod issue

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
+// app.use(express.json());
+// app.use(
+//   express.urlencoded({
+//     extended: false,
+//   })
+// );
 
 // connectDB();
 
