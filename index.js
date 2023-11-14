0f2424b70a9df0e7db7246e0eb5e810853aadc5a
gowthamkish <gowthamkish@outlook.com>
2023-11-14 09:55:11 +0530

    Fixing prod issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
-// const userRoute = require("./routes/userRoutes");
+const userRoute = require("./routes/userRoutes");
 // const connectDB = require("./config/db");
 
 const app = express();
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
 
-// app.use("/api/users", userRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT ${PORT} `);
