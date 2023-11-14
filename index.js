8f02c139fda696d3558d977d7c83afc51367abea
gowthamkish <gowthamkish@outlook.com>
2023-11-14 09:42:24 +0530

    Fixing prod issue

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
-const userRoute = require("./routes/userRoutes");
-const connectDB = require("./config/db");
+// const userRoute = require("./routes/userRoutes");
+// const connectDB = require("./config/db");
 
 const app = express();
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
+// app.use(express.json());
+// app.use(
+//   express.urlencoded({
+//     extended: false,
+//   })
+// );
 
 // connectDB();
 
